95f8bab31310566c02c4bada86bef5b478fb2ab1
Victor Andre <andre.victor60@free.fr>
2021-03-18 14:19:54 +0100

    Ajout des skeletons controllers

---
+++ src/main/java/fr/dawan/controllers/EntrepriseController.java
@@ -0,0 +1,10 @@
+package fr.dawan.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/entreprises")
+public class EntrepriseController {
+
+}
---
+++ src/main/java/fr/dawan/controllers/PosteController.java
@@ -0,0 +1,10 @@
+package fr.dawan.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/postes")
+public class PosteController {
+
+}
